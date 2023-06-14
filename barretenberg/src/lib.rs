3db550d12cd1b7d3462ac148786759ac1a30dbab
TheMenko <themenkoprojects@gmail.com>
2023-06-14 22:28:10 +0200

    something something run

--- barretenberg/src/lib.rs
+++ barretenberg/src/lib.rs
@@ -1,5 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+pub fn run(length: usize) {
+    unsafe { unimplemented!() }
 }
 
 #[cfg(test)]
@@ -8,7 +9,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        //
     }
 }
